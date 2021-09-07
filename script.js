948aab718a0120f410a6f4fa17056ab1db695188
sjlarrain <sjlarrain@uc.cl>
2021-09-07 11:04:58 -0300

    Chart

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
+
 const interpreter = (d) => {
-    return {date: parseDate(d.date),
+    return {date: d3.timeParse("%d-%m-%Y")(d.date),
             value: parseFloat(d.value)
     }
 }
 
 const size = {
-    width: 300,
+    width: 1000,
     height: 300
 }
 
@@ -16,16 +17,46 @@ const margin = {
     bottom: 10
 }
 
-const svg = d3.selectAll(".graph")
+const svgframe = d3.selectAll(".graph")
             .append("svg")
             .attr("class", 'rent')
             .attr("width", size.width)
             .attr("height", size.height)
 
 const graph = (datos) => {
-    const labels = datos.value
-    const g = detalle.append("g")
+    // const labels = datos.value
+    const g = svgframe.append("g")
     const margin = {top: 30, bottom: 30, left: 30, right: 30}
     const height = size.height - margin.top - margin.bottom
     const width = size.width -margin.left - margin.right
+    let x = d3.scaleTime()
+               .domain(d3.extent(datos, (d)=>{return d.date}))
+               .range([0, width])
+    g.append("g")
+    .attr("transform", "translate(0," + height + ")")
+    .call(d3.axisBottom(x));
+
+    let y = d3.scaleLinear()
+                .domain([0, d3.max(datos, (d)=>{return d.value})])
+                .range([height, 0])
+    g.append("g")
+      .call(d3.axisLeft(y))
+
+    g.append("path")
+    .datum(datos)
+      .attr("fill", "none")
+      .attr("stroke", "steelblue")
+      .attr("stroke-width", 1.5)
+      .attr("d", d3.line()
+        .x(function(d) { return x(d.date) })
+        .y(function(d) { return y(d.value) })
+        )
 }
+
+
+d3.csv("data1.csv", interpreter).then((datos) => {
+  graph(datos)
+datos.forEach((d)=>console.log(d))  
+}).catch((err) => {
+    console.log(err)
+})
